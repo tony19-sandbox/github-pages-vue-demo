523a5fb077b145539e0c3edfcb3a22a35d70aed8
Tony Trinh <tony19@gmail.com>
2020-05-18 00:05:25 -0500

    log package version

--- src/main.js
+++ src/main.js
@@ -8,7 +8,7 @@ new Vue({
   router,
   render: h => h(App),
   created() {
-    console.log('app created')
+    console.log('app version', process.env.VUE_APP_VERSION)
     if (sessionStorage.redirect) {
       const redirect = sessionStorage.redirect
       console.log('redirect', redirect)
